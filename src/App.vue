2ef16212b6ba701375fe88d69b51a261dd0121cd
Konami Ota <konami.backham@gmail.com>
2021-11-15 16:35:45 +0900

    make three-vue project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+    #app
+        the-canvas
+</template>
+
+<script>
+import TheCanvas from './components/TheCanvas'
+
+export default {
+    name: 'App',
+    components: {
+        TheCanvas,
+    },
+}
+</script>
+<style lang="scss" src="./assets/css/app.css"></style>
+
